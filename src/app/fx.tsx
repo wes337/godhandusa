af0c6a95f06f309c4ec959ab7a2e35a7d0bb2590
wes337 <wesmoses@gmail.com>
2025-05-07 15:15:01 +0300

    css stuff

--- src/app/fx.tsx
+++ src/app/fx.tsx
@@ -1,12 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
-
 export default function FX({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    document.body.classList.add("on");
-  }, []);
-
   return (
     <>
       <div className="overlay" />
